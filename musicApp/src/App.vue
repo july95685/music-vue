b88daf83a3dbc6aebe2b14372f4f2ea80214b362
“july_haidingPC <july95685@gmail.com>
2018-03-09 20:09:30 +0800

    no message

---
+++ musicApp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+export default {
+  name: 'App',
+  components:{
+  	Header
+  }
+}
+</script>
+
+<style>
+@import "common/stylus/icon.styl"
+</style>
---
+++ musicApp/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <i class="icon-back"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "../common/stylus/variable"
+@import "../common/stylus/mixin"
+
+.hello
+  background: $f7f7f7;
+  h2
+    color:red;
+  .icon-back
+    display: block
+    padding: 10px
+    font-size: $font-size-large-x
+    color: $color-theme
+</style>
