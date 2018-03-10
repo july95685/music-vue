1af5d2bba87dcb060726c88983dd35409f4627fe
july-mac <july95685@gmail.com>
2018-03-10 15:29:25 +0800

    no message

--- musicApp/src/components/header.vue
+++ musicApp/src/components/header.vue
@@ -9,5 +9,24 @@
 export default {}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
+	@import "../common/stylus/variable"
 
+	.m-header
+		width:100%
+		text-align:center
+		height:44px
+		.icon
+			background-size:100% 100%
+			display:inline-block
+			width:30px;
+			height:32px;
+			margin-top:6px
+			margin-right:9px
+			background-image:url('../common/image/default.png')
+		.text
+			vertical-align:top
+			line-height:44px
+			display:inline-block
+			color:$color-theme
+			font-size:18px
 </style>
